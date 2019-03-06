91c141eb06090423fcf104282b0e50adaf2f71c3
bmsheedy <bmsheedy@buffalo.edu>
2019-03-05 22:46:23 -0500

    added two JS files

--- src/main.js
+++ src/main.js
@@ -12,11 +12,11 @@ function setup() {
 function draw() {
     background(0);
 
-    translate(width/2, height/2);
-    var newzoom = 64 / player.r;
-    zoom = lerp(zoom, newzoom, 0.1);
-    scale(zoom);
-    translate(-player.pos.x, -player.pos.y);
+    //translate(width/2, height/2);
+    //var newzoom = 64 / player.r;
+    //zoom = lerp(zoom, newzoom, 0.1);
+    //scale(zoom);
+    //translate(-player.pos.x, -player.pos.y);
 
     player.show();
     player.update();
