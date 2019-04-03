b7393bf8846beda3cf283bdfd8464e71c0f91fcb
bmsheedy <bmsheedy@buffalo.edu>
2019-04-02 23:17:49 -0400

    color

--- src/food.js
+++ src/food.js
@@ -2,10 +2,37 @@ function food(x, y, rad) {
     this.vec = createVector(x, y);
     this.radius = rad;
 
-    var r1 =
+    var c = color(255)
+    var red = color(255, 0, 0)
+    var green = color(0, 255, 0)
+    var blue = color(0, 0, 255)
+    var yellow = color(255, 255, 0)
+    var orange = color(255, 127, 0)
+    var purple = color(128, 0, 128)
+
+    var n = Math.floor((Math.random() * 6) + 1)
+
+    if(n == 1){
+        c = red
+    }
+    else if(n == 2){
+        c = orange
+    }
+    else if(n == 3){
+        c = yellow
+    }
+    else if(n == 4){
+        c = green
+    }
+    else if(n == 5){
+        c = blue
+    }
+    else{
+        c = purple
+    }
 
     this.show = function() {
-        fill(255);
+        fill(c);
         ellipse(this.vec.x, this.vec.y, this.radius*2, this.radius*2);
     }
 }
