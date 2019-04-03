e61f70048c38d32923950af81a8013c465e4cbda
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 00:04:34 -0400

    minor adjustments and clarifications

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ var fList = [];
 socket = io.connect('http://localhost:43343');
 
 function setup() {
-    createCanvas(1200, 800);        //sets up the world
+    createCanvas(windowWidth, windowHeight);        //sets up the world
     player = new Player(width/2,height/2, 20);      //creates the player
     var t = 0;
-    while(t < 200){     //randomly generates food. later must add ways to add food if the world runs low on food.
-        fList[t] = new food(random(width), random(height), 10);     //creates food circles
+    while(t < 2000){     //randomly generates food. later must add ways to add food if the world runs low on food.
+        fList[t] = new food(random(-width, width), random(-height, height), 5);     //creates food circles
         t += 1;
     }
 }
@@ -20,8 +20,8 @@ function draw() {
     //below edits the list of food we have and removes food that is eaten from the world fro the food list
     for (c = fList.length - 1; c >= 0; c--) {   //loops through descending order of the food
         fList[c].show();
-        if(player.eats(fList[c])) {
-            fList.splice(c, 1);
+        if(player.eatFood(fList[c])) {
+            fList.splice(c, 1);     //removes food from the world if food is eaten
         }
     }
 
--- src/player.js
+++ src/player.js
@@ -5,20 +5,20 @@ function Player(x, y, rad) {        //constructor for players
 
     this.update = function() {  //updates position
         var velocity = createVector(mouseX-width/2, mouseY-height/2);
-        velocity.setMag(3); //speed of which we move
-        this.velocity.lerp(velocity, 0.2);
+        velocity.setMag(4); //speed of which we move
+        this.velocity.lerp(velocity, 0.2);      //lerp stands for Linear Interpolation. it tells the item to follow the mouse
         this.vec.add(this.velocity);
     }
 
     this.show = function() {    //updates display
-        fill(255, 0, 0);
+        fill(255, 255, 255);  //color of player
         ellipse(this.vec.x, this.vec.y, this.radius*2, this.radius*2);
     }
     //below determines what happens when you eat food
-    this.eats = function(f) {
+    this.eatFood = function(f) {
         var dis = p5.Vector.dist(this.vec, f.vec);  //p5 documentation for vector distances
         if(dis < this.radius + f.radius){   //compares to see if the ball collides with food
-            this.radius += (f.radius * 0.1);    //this determines how big the player should grow everytime you eat food.
+            this.radius += (f.radius * 0.05);    //this determines how big the player should grow everytime you eat food.
             return true;
         }
         else{
